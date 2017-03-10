77be56f2a6c538262ec127a3c8eefb2863b22dd1
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-03-10 21:54:14 +0000

    Condense and fix missing \n from args

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-fn fw(s: &mut String) -> String {
+fn main() {
     let fw = |c: char| if c.is_alphabetic() {
         std::char::from_u32(c as u32 + 65248)
     } else {
          match c { ' ' => Some('　'), _ => Some(c), }
     };
-    s.drain(..).filter_map(&fw).collect()
-}
-
-fn main() {
-    let pretty_print = |mut s| print!("{}", fw(&mut s));
+	let pretty_print = |mut s: String| println!("{}", s.drain(..).filter_map(&fw).collect::<String>());
     if !std::env::args().skip(1).map(&pretty_print).collect::<Vec<_>>().is_empty() { return }
-    let flatten_line = |line: std::io::Result<_>| line.unwrap_or("".to_string()) + "\n";
+    let flatten_line = |line: std::io::Result<_>| line.unwrap_or("".to_string());
     use std::io::BufRead;
     let stdin = std::io::stdin();
     stdin.lock().lines().map(&flatten_line).map(&pretty_print).collect::<Vec<_>>();
