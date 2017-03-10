788ca13e15f645e8d0722a6e779e47c5fd7094a1
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-03-10 22:17:54 +0000

    Format: tabs -> spaces

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ fn main() {
     } else {
          match c { ' ' => Some('　'), _ => Some(c), }
     };
-	let pretty_print = |mut s: String| println!("{}", s.drain(..).filter_map(&fw).collect::<String>());
+    let pretty_print = |mut s: String| println!("{}", s.drain(..).filter_map(&fw).collect::<String>());
     if std::env::args().skip(1).map(&pretty_print).collect::<Vec<_>>().is_empty() {
         let flatten_line = |line: std::io::Result<_>| line.unwrap_or("".to_string());
         use std::io::BufRead;
         let stdin = std::io::stdin();
         stdin.lock().lines().map(&flatten_line).map(&pretty_print).collect::<Vec<_>>();
-	}
+    }
 }
