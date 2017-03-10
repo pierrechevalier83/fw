df6a0cfd8f5ac563d17a31014cc96047876bb635
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-03-10 21:46:45 +0000

    Condense

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
 fn fw(s: &mut String) -> String {
-    s.drain(..)
-        .filter_map(|c| if c.is_alphabetic() {
-                        std::char::from_u32(c as u32 + 65248)
-                    } else {
-                        match c {
-                            ' ' => Some('　'),
-                            _ => Some(c),
-                        }
-                    })
-        .collect()
+    let fw = |c: char| if c.is_alphabetic() {
+        std::char::from_u32(c as u32 + 65248)
+    } else {
+         match c { ' ' => Some('　'), _ => Some(c), }
+    };
+    s.drain(..).filter_map(&fw).collect()
 }
 
 fn main() {
-    let pretty_print = |mut s| {
-        print!("{}", fw(&mut s));
-    };
-    let args = std::env::args().skip(1).map(pretty_print).collect::<Vec<_>>();
-    if args.is_empty() {
-        use std::io::BufRead;
-        let stdin = std::io::stdin();
-        stdin.lock()
-            .lines()
-            .map(|line| line.unwrap_or("".to_string()) + "\n")
-            .map(pretty_print)
-            .collect::<Vec<_>>();
-    }
+    let pretty_print = |mut s| print!("{}", fw(&mut s));
+    if !std::env::args().skip(1).map(&pretty_print).collect::<Vec<_>>().is_empty() { return }
+    let flatten_line = |line: std::io::Result<_>| line.unwrap_or("".to_string()) + "\n";
+    use std::io::BufRead;
+    let stdin = std::io::stdin();
+    stdin.lock().lines().map(&flatten_line).map(&pretty_print).collect::<Vec<_>>();
 }
