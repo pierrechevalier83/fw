18cab68b98eb0fd0bf5f2f18b8a2f175b57b29e8
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-03-10 22:10:00 +0000

    Rereading the challenge, it should match any ascii char

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    let fw = |c: char| if c.is_alphanumeric() {
+    use std::ascii::AsciiExt;
+    let fw = |c: char| if c.is_ascii() {
         std::char::from_u32(c as u32 + 65248)
     } else {
          match c { ' ' => Some('　'), _ => Some(c), }
